9ce1cfc24710aad12c31baf2f88607e9bb948df1
satyasaranu <saranusys@gmail.com>
2023-04-13 19:50:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-06e46074ae430fba6"
   instance_type = "t2.micro"
 
   tags = {
